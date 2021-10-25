d4ec9aaacae4d1d5f9ce85aebd5601122329b9bd
Rian <49768896+DatRatVS@users.noreply.github.com>
2021-10-24 23:43:28 -0300

    Adding another stupid legend.

--- BrawlhallaRandom.py
+++ BrawlhallaRandom.py
@@ -5,7 +5,7 @@ legends = ["Bödvar", "Cassidy", "Orion", "Lord Vraxx", "Gnash", "Queen Nai", "H
           "Azoth", "Koji", "Ulgrim", "Diana", "Jhala", "Kor", "Wu Shang", "Val", "Ragnir", "Cross", "Mirage",
           "Nix", "Mordex", "Yumiko", "Artemis", "Caspian", "Sidra", "Xull", "Kaya", "Isaiah", "Jiro", "Lin Fei",
           "Zariel", "Rayman", "Dusk", "Fait", "Thor", "Petra", "Vector", "Volkov", "Onyx", "Jaeyun", "Mako",
-          "Magyar", "Reno"]
+          "Magyar", "Reno", "Munin"]
 
 i = 1
 inp = int(input("How many times you want it to be rolled? "))
